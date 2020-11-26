e220de695c3294ee91e98c85dad642f99ba7377e
genie <gnaltm@gmail.com>
2020-11-26 11:00:04 +0900

    지니

---
+++ algo/step-1/bn/algo/genie/Step1.java
@@ -0,0 +1,13 @@
+package bn.algo.genie;
+
+public class Step1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		problem1();
+	}
+	
+	public static void problem1() {
+		System.out.println("Hello World!");
+	}
+}
