a7dc7a69a45ae8b64e2f779355b878d7998f9aca
hyeon21 <runngun21@naver.com>
2020-12-20 17:45:39 +0900

    Gromit 스텝8 시작

---
+++ gromit/bn/algo/gromit/Step8.java
@@ -0,0 +1,27 @@
+package bn.algo.gromit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Step8 {
+
+	public static void main(String[] args) throws IOException {
+		getBreakEvenPoint();
+	}
+	
+	public static void getBreakEvenPoint() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] info = br.readLine().split(" ");
+		final int FIXED_COST = Integer.parseInt(info[0]);
+		final int VARIABLE_COST = Integer.parseInt(info[1]);
+		final int GOODS_COST = Integer.parseInt(info[2]);
+		br.close();
+		if (GOODS_COST <= VARIABLE_COST) {
+			System.out.println("-1");
+			return;
+		}
+		System.out.println((FIXED_COST / (GOODS_COST - VARIABLE_COST)) + 1);
+	}
+}
